fa9555f4978cbbbaff6ab00b481714b6aabf9ef2
Joshua Samuel <joshuasam365@gmail.com>
2025-08-04 16:25:42 +0530

    vercel debug -js module

--- server-u/index.js
+++ server-u/index.js
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/test", (req, res) => res.send("Working"));
-app.use("/", urlRoutes);
-app.use("/auth", authRoutes);
+app.use("/api/", urlRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
